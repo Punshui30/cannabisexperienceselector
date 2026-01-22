fd8f76699128f75f5a3e67d53cbf7a398db069c4
Punshui30 <simmonds80@gmail.com>
2026-01-21 20:30:17 -0500

    V9.0.1: Forced redeployment trigger

--- src/App.tsx
+++ src/App.tsx
@@ -390,7 +390,7 @@ export default function App() {
                   />
                 )}
                 <div className="fixed bottom-2 right-2 text-[8px] text-white/20 pointer-events-none flex flex-col items-end gap-1">
-                  <span>v9.0 (FINAL)</span>
+                  <span>v9.0.1 (DEPLOY_FORCE)</span>
                   {debugLog.map((l, i) => <span key={i} className="opacity-50">{l}</span>)}
                 </div>
 
